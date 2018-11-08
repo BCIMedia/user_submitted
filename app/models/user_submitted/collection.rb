fc8ad94c36506dabe11916b9b415fd3d21ea8b5c
Skylar Bolton <sbolton@bcimedia.com>
2018-11-08 15:15:55 -0700

    Preview first steps

--- app/models/user_submitted/collection.rb
+++ app/models/user_submitted/collection.rb
@@ -13,5 +13,9 @@ module UserSubmitted
 
     scope :approved_contents, -> { joins(:contents).where(user_submitted_contents: {status: Content.statuses[:approved]}).references(:contents) }
 
+
+  def to_param
+    "#{id}-#{name.urlify if name}"
+  end
   end
 end
